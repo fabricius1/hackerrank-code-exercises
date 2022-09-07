a93efa3f8dabc7c23068ee5cafd10396e254f4bc
fabricius1 <fabriciusbr@gmail.com>
2022-09-07 11:08:37 -0300

    "The Report" solved

--- sql/medium_difficulty.sql
+++ sql/medium_difficulty.sql
@@ -1,3 +1,4 @@
+-- The PADS
 -- https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
 
 -- CODE 1
@@ -28,6 +29,7 @@ ORDER BY COUNT(Occupation);
 
 /*****************************************************/
 
+-- Top Competitors
 -- https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true
 
 -- CODE 1
@@ -64,3 +66,50 @@ ORDER BY SUM(
         ELSE 0
     END) DESC, 1;
 
+/*****************************************************/
+
+-- The Report
+-- https://www.hackerrank.com/challenges/the-report/problem?isFullScreen=true
+
+-- CODE 1
+SELECT
+    IIF(Marks <= 69, NULL, Name),
+    CASE
+        WHEN Marks BETWEEN 0 AND 9 THEN 1
+        WHEN Marks BETWEEN 10 AND 19 THEN 2
+        WHEN Marks BETWEEN 20 AND 29 THEN 3
+        WHEN Marks BETWEEN 30 AND 39 THEN 4
+        WHEN Marks BETWEEN 40 AND 49 THEN 5
+        WHEN Marks BETWEEN 50 AND 59 THEN 6
+        WHEN Marks BETWEEN 60 AND 69 THEN 7
+        WHEN Marks BETWEEN 70 AND 79 THEN 8
+        WHEN Marks BETWEEN 80 AND 89 THEN 9
+        WHEN Marks BETWEEN 90 AND 100 THEN 10
+    END AS Grades,
+    Marks
+FROM STUDENTS
+ORDER BY 2 DESC, 1
+
+-- CODE 2
+SELECT
+    CASE
+        WHEN Marks <= 69 THEN NULL
+        ELSE Name
+    END AS Masked_Names,
+    CASE
+        WHEN Marks BETWEEN 0 AND 9 THEN 1
+        WHEN Marks BETWEEN 10 AND 19 THEN 2
+        WHEN Marks BETWEEN 20 AND 29 THEN 3
+        WHEN Marks BETWEEN 30 AND 39 THEN 4
+        WHEN Marks BETWEEN 40 AND 49 THEN 5
+        WHEN Marks BETWEEN 50 AND 59 THEN 6
+        WHEN Marks BETWEEN 60 AND 69 THEN 7
+        WHEN Marks BETWEEN 70 AND 79 THEN 8
+        WHEN Marks BETWEEN 80 AND 89 THEN 9
+        WHEN Marks BETWEEN 90 AND 100 THEN 10
+    END AS Grades,
+    Marks
+FROM STUDENTS
+ORDER BY 2 DESC, 1
+
+/*****************************************************/
